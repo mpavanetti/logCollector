7f9928d093a5eaa121f2acaaf5395e5135a5ece1
Matheus Pavanetti <matheuspavanetti@gmail.com>
2021-08-10 12:06:49 -0300

    Updating uninstall.bat

--- agent/uninstall.bat
+++ agent/uninstall.bat
@@ -1,9 +1,15 @@
 @ECHO OFF
 @TITLE Talend Python Log Collector Uninstaller
 echo by Matheus Pavanetti - 2021
+echo Stoping TalendPyLogCollector windows service...
+%~dp0installers\nssm.exe stop TalendPyLogCollector
+echo.
 echo Removing TalendPyLogCollector service...
 %~dp0installers\nssm.exe remove TalendPyLogCollector confirm
 echo Service has been deleted with success !
+echo.
+echo Deleting TalendEnv python virtual environment...
+rmdir /s /q C:\Python\Environments\TalendEnv\
 echo.
 echo Please, close this window !
 echo.
